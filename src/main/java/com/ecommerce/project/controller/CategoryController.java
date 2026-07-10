eb1b8deec9c8a1ba77979b719460264c30291978
Gautam Gupta <ggautamg23@gmail.com>
2026-07-10 15:09:12 +0530

    E-Commerce Project

---
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.model.Category;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CategoryController {
+
+    private List<Category> categories = new ArrayList<>();
+
+    @GetMapping("/api/public/categories")
+    public List<Category> getAllCategories() {
+        return categories;
+    }
+
+    @PostMapping("/api/public/categories")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);
+        return "Categories added successfully";
+    }
+}
